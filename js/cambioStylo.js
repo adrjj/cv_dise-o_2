41af600d884f5f3d262a9f555fee88bd855af67d
Adriano J. Albarran <adrianojosealbarran@gmail.com>
2023-10-19 15:10:19 -0300

    version1.0

---
+++ js/cambioStylo.js
@@ -0,0 +1,12 @@
+  function cambiarEstilo() {
+     let estilo = document.getElementById("estilo-css");
+      if (estilo.href.endsWith("stylo.css")) {
+          estilo.href = "./css/stylorecluted.css"; // Cambia a tu segundo archivo CSS
+      } else {
+           estilo.href = "./css/stylo.css"; // Cambia de regreso a tu primer archivo CSS
+      }
+ }
+
+
+
+
